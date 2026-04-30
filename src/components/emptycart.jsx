f165a5be5e17dfc70db43e9b95f8a59ead1c4cc7
Ceeessan <Cessan__94@hotmail.com>
2026-04-30 22:33:20 +0200

    Adding old project

---
+++ src/components/emptycart.jsx
@@ -0,0 +1,5 @@
+export default function EmptyCart() {
+    return (
+        <h3 id="empty-cart"> Din varukorg är tom </h3>
+    )
+}
---
+++ src/components/thankyoucard.jsx
@@ -0,0 +1,8 @@
+export default function ThankYouCard() {
+
+    return (
+        <div>
+            <h1 id="thank-you-card"> Thank you for your purchase!</h1>
+        </div>
+    )
+}
